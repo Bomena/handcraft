b3d2d98bb09368cfd8d03ca57248c2f8c28f9f26
bomenakim <96710675+Bomena@users.noreply.github.com>
2023-10-11 03:03:33 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 this.ros = new ROSLIB.Ros({
-    url : 'ws://localhost:3000'
+    url : 'ws://localhost:9090'
 });
 this.ros.on('connection', function() {
     console.log('Connected to websocket server.');
